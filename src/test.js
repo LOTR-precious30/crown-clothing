25faf4854435d033bd007ce6615f9fe1afee0f67
LOTR-precious30 <prasannapudasaini160@gmail.com>
2022-02-01 13:30:28 +0545

    Last changes sign up

---
+++ src/test.js
@@ -0,0 +1,7 @@
+import firebase from "firebase/compat/app";
+import 'firebase/compat/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('fMQUTKyHRPFVWTF3vopq').collection('cartItems').doc('Znt7rQSSeYCN9uIHhEMn');
+firestore.doc('/users/fMQUTKyHRPFVWTF3vopq/cartItems/Znt7rQSSeYCN9uIHhEMn');

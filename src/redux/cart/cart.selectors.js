c022b2473897c4d1872d6bd8e8f7b19ea465e7b1
LOTR-precious30 <prasannapudasaini160@gmail.com>
2022-02-02 17:39:08 +0545

    cart item count

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,17 @@
+// import { createDispatchHook } from 'react-redux';
+// import { createSelector } from 'reselect';
+
+// const selectCart = state => state.cart;
+
+// export const selectCartItems = createSelector(
+//     [selectCart],
+//     (cart) => createDispatchHook.cartItems
+// );
+
+// export const selectCartItemsCount = createSelector(
+//     [selectCartItems],
+//     cartItems => 
+//      cartItems.reduce(
+//         (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+//      )
+// )

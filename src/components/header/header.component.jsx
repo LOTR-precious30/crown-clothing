556f3c15325424edbc556934b4a2b6466878118e
LOTR-precious30 <prasannapudasaini160@gmail.com>
2022-02-02 14:39:14 +0545

    cart icon component with its redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,11 @@ import { auth } from '../../firebase/firebase.util'
 
 import { connect } from 'react-redux';
 
-const Header = ({ currentUser }) => (
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to = "/">
             <Logo className='logo' />
@@ -26,12 +30,15 @@ const Header = ({ currentUser }) => (
                     Sign in
                 </Link>
             }
+            <CartIcon />
         </div>
+        {hidden ? (null) : (<CartDropdown />)}
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
